970bd13f6814ea1a492651402b59705626ec3af9
HarveyPost <sc23hp2@leeds.ac.uk>
2026-02-15 14:09:15 +0000

    fuzzy finder updated

--- lua/harvey/plugins/telescope.lua
+++ lua/harvey/plugins/telescope.lua
@@ -1,4 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
+    }
 }
