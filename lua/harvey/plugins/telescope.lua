2f4a08767e420366b9565aca278d21aa45e52c64
HarveyPost <sc23hp2@leeds.ac.uk>
2026-02-15 14:20:22 +0000

    fix: fuzzy finder fully fixed

--- lua/harvey/plugins/telescope.lua
+++ lua/harvey/plugins/telescope.lua
@@ -1,7 +1,47 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
+    'nvim-telescope/telescope.nvim',
+    version = '*',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
-    }
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+        local telescope = require('telescope')
+
+        telescope.setup({
+            defaults = {
+                vimgrep_arguments = {
+                    'rg',
+                    '--color=never',
+                    '--no-heading',
+                    '--with-filename',
+                    '--line-number',
+                    '--column',
+                    '--smart-case',
+                    '--hidden',
+                    '--no-ignore',
+                    '--no-ignore-parent',
+                    '--follow',
+                },
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    no_ignore = true,
+                    no_ignore_parent = true,
+                    follow = true,
+                    find_command = {
+                        'rg',
+                        '--files',
+                        '--hidden',
+                        '--no-ignore',
+                        '--no-ignore-parent',
+                        '--follow',
+                    },
+                },
+            },
+        })
+
+        pcall(telescope.load_extension, 'fzf')
+    end,
 }
